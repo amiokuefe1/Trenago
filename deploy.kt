bb358f2e960b1fd4101428cd1d7fcbf90bdb465f
_E.A||E.J_ <48546116+amiokuefe1@users.noreply.github.com>
2021-06-07 11:19:38 +0100

    Added Kotlin lines of code

---
+++ deploy.kt
@@ -0,0 +1,25 @@
+5 Functions without Return Value.
+
+fun main() {
+    println("Hello, Tolu!!!")
+    hello()
+    age()
+    job()
+    country()
+} // End of Main Function
+
+fun hello() {
+    print("My name is Efe. ")
+}
+
+fun age() {
+    print("I am in my late 20s, ")
+}
+
+fun job() {
+    print("soon, I will become an Android Developer. ")
+}
+
+fun country() {
+    print("last but not least, I'm from the United African Republic.")
+}
